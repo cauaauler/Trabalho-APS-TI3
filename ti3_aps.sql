b3cd534d8bca5274a94876cf652ca4a10fa1b150
Kauã Klassmann <kaua.klassmann661@gmail.com>
2024-11-13 18:32:27 -0300

    crud de residuos: Cadastrar; BANCO

---
+++ ti3_aps.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Nov 13, 2024 at 10:31 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ti3_aps`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `residuo`
+--
+
+CREATE TABLE `residuo` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `descricao` text NOT NULL,
+  `foto` varchar(255) NOT NULL,
+  `tipo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tipo_residuo`
+--
+
+CREATE TABLE `tipo_residuo` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `residuo`
+--
+ALTER TABLE `residuo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `tipo` (`tipo`);
+
+--
+-- Indexes for table `tipo_residuo`
+--
+ALTER TABLE `tipo_residuo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `residuo`
+--
+ALTER TABLE `residuo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tipo_residuo`
+--
+ALTER TABLE `tipo_residuo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `residuo`
+--
+ALTER TABLE `residuo`
+  ADD CONSTRAINT `residuo_ibfk_1` FOREIGN KEY (`tipo`) REFERENCES `tipo_residuo` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
